363c5ba0bbdb72d6e15e274e13b0a6761c085216
Maria Fernanda♡ Medina Vega <64274136+Maffemedina@users.noreply.github.com>
2022-11-19 18:25:24 -0500

    Add files via upload

---
+++ php/conexion.php
@@ -0,0 +1,9 @@
+<?php
+  $Servidor ="localhost";
+  $Base_Datos= "ProyectoBD";
+  $usuario= "root";
+  $clave= "Qlarinetemaffe2";
+
+
+  $conexion = new mysqli($Servidor,$usuario,$clave);
+  mysqli_select_db($conexion,$Base_Datos);
