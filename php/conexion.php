336d8a83f2d5d45989945a19116d2610bad2f756
Maffemedina <mmedina08845@unversidadean.edu.co>
2022-11-19 19:21:46 -0500

    Creando la funcion retornaDatosCliente

--- php/conexion.php
+++ php/conexion.php
@@ -4,5 +4,7 @@
   $usuario= "root";
   $clave= "Qlarinetemaffe2";
 
-  $conexion =new mysqli($Servidor,$usuario,$clave);
+
+  $conexion = new mysqli($Servidor,$usuario,$clave);
   mysqli_select_db($conexion,$Base_Datos);
+//
