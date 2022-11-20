22b561f8842ad5dc76498e4b6e27a20642a6f8c0
Maria Fernanda♡ Medina Vega <64274136+Maffemedina@users.noreply.github.com>
2022-11-20 16:19:39 -0500

    Delete conexion.php

--- php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $Servidor ="localhost";
-  $Base_Datos= "ProyectoBD";
-  $usuario= "root";
-  $clave= "Qlarinetemaffe2";
-
-
-  $conexion = new mysqli($Servidor,$usuario,$clave);
-  mysqli_select_db($conexion,$Base_Datos);
